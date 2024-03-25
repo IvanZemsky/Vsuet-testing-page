7679e890d61e62b072e3b1665843a7e5a960c9b1
IvanZemsky <i.ivanzemsky2005@gmail.com>
2024-03-25 15:08:36 +0300

    result page 2

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-import "./styles/App.css";
-import "./styles/fonts.css";
-import "./styles/wrapper.css";
-import "./styles/_vars.css";
+import "./App.css";
+import "../../styles/fonts.css";
+import "../../styles/wrapper.css";
+import "../../styles/_vars.css";
 import { Route, Routes } from "react-router-dom";
-import {LazyTest as Test} from "./pages/Test/LazyTest";
-import {LazyResult as Result} from "./pages/Result/LazyResult";
+import {LazyTest as Test} from "../../pages/Test/LazyTest";
+import {LazyResult as Result} from "../../pages/Result/LazyResult";
 import { useSelector } from "react-redux";
-import Layout from "./components/Layout/Layout";
+import Layout from "../Layout/Layout";
 
 const App = () => {
    const theme = useSelector((state) => state.theme.theme);
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/reset.css";
-import App from "./App";
+import App from "./components/App/App";
 import { BrowserRouter, HashRouter } from "react-router-dom";
 import store, { persistor} from "./store/store";
 import { Provider } from "react-redux";
