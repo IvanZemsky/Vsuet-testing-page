aa8daa7a340b7fcc6903b49e26e865736ea9cda8
IvanZemsky <i.ivanzemsky2005@gmail.com>
2024-04-02 13:35:24 +0300

    new background

--- src/components/UI/Preview/Preview.jsx
+++ src/components/UI/Preview/Preview.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import styles from "./Preview.module.css"
-import building from "../../../assets/img/building.webp";
+import background from "../../../assets/img/background.webp";
 
 const Preview = () => {
    return (
       <div className={styles.preview}>
          <div className={styles.imgWrap}>
             <img
-               src={building}
+               src={background}
                alt="Учебный корпус ВГУИТ, проспект Революции, 19"
             />
          </div>
