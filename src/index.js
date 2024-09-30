987d635896d21a56c957c865e6e9e063934d7681
IvanZemsky <i.ivanzemsky2005@gmail.com>
2024-09-30 20:32:10 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -11,9 +11,9 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
    <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-         <HashRouter>
+         <BrowserRouter>
             <App />
-         </HashRouter>
+         </BrowserRouter>
       </PersistGate>
    </Provider>
 );
