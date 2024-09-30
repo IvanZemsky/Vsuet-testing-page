20bbff04f0295f8938c9ac65dcb805869981757c
IvanZemsky <i.ivanzemsky2005@gmail.com>
2024-09-30 20:55:00 +0300

    gix

--- src/index.js
+++ src/index.js
@@ -9,11 +9,11 @@ import { PersistGate } from "redux-persist/integration/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-   <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-         <HashRouter>
-            <App />
-         </HashRouter>
-      </PersistGate>
-   </Provider>
-);
+   <HashRouter>
+     <Provider store={store}>
+       <PersistGate loading={null} persistor={persistor}>
+         <App />
+       </PersistGate>
+     </Provider>
+   </HashRouter>
+ );
